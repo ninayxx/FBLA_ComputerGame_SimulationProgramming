56c54569d78f34a2c110ed97aba4da8c6bcbc899
Nina Xu <ninayxx@gmail.com>
2026-02-03 20:15:41 -0800

    Start button functionality

--- js/main.js
+++ js/main.js
@@ -9,3 +9,21 @@ instructions_button.addEventListener('click', () => {
 close_instructions_button.addEventListener('click', () => {
     instructions_modal.classList.remove('show-modal');
 });
+
+function changeScreen() {
+    const startScreen = document.getElementById('start-screen');
+    const jobSelectScreen = document.getElementById('job-select');
+    const mainContainer = document.getElementById('mainContainer');
+    const bodyContainer = document.getElementById('bodyContainer');
+    const header = document.querySelector('header');
+
+    startScreen.style.display = 'none';
+    header.style.display = 'none';
+    jobSelectScreen.style.display = 'block';
+    bodyContainer.style.backgroundImage = 'url(../assets/images/locations/job-selection-background.png)';
+    bodyContainer.style.backgroundSize = '100% 100%';
+    bodyContainer.style.alignItems = 'flex-start';
+    bodyContainer.style.paddingTop = '50px';
+    mainContainer.style.backgroundColor = 'transparent';
+    mainContainer.style.boxShadow = 'none';
+}
